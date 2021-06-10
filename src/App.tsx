3be8146c665b01eb736dc126dcaad0c66536bb89
Daniel Motta Ruiz Garcia <daniel.motta@CNTAPPLE3W3ML7H.local>
2021-06-10 13:37:28 -0300

    commit project react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useForm, SubmitHandler } from "react-hook-form";
 
-function App() {
+type InputsProps = {
+  name: string,
+  age: number,
+}
+
+const App: React.FC = () => {
+
+  const {register, handleSubmit, formState: {errors} } = useForm<InputsProps>();
+
+  const onSubmit: SubmitHandler<InputsProps> = data => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <h2>Formulários</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input 
+          type="text"
+          placeholder="Name..."
+          className="form-control mb-2"
+         {...register('name', {required: {
+           value: true,
+           message: 'Todos os campos precisam ser preenchidos!'
+          },
+          minLength: {
+            value: 5,
+            message: 'O campo precisa ter no mínimo 5 dígitos!'
+          }
+         })}
+        />
+
+        <input 
+          type="number"
+          placeholder="Age..."
+          className="form-control mb-2"
+          {...register('age', { required: {
+            value: true,
+            message: 'Preencha todos os campos por favor!'
+          }, 
+        })}
+        />
+
+
+        {errors.name && <span className="text-danger mx-auto">{errors.name.message}</span>}<br />
+        {errors.age && <span className="text-danger mx-auto">{errors.age.message}</span>}<br />
+
+        <button className="btn btn-primary btn-block" type="submit">Submit</button>
+      </form>
+    </div>    
   );
 }
 
-export default App;
+export default App; 
